4285e7b206333f15a2708c727e135891fc9edb1a
elbertwidjaja <elbertwidjaja07@gmail.com>
2024-01-15 23:00:03 +0700

    finish banner component

--- app/components/BannerHome/index.tsx
+++ app/components/BannerHome/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import jumbotron from "../../../public/jumbotron.jpeg";
 import styles from "./styles.module.scss";
+import Image from "next/image";
 
 function BannerHome() {
   return (
     <div className={styles.overlay}>
-      <img src={} alt="Banner" className={styles.bannerImage} />
-      <div className={styles.bannerText}>Your Banner Text</div>
+      <Image
+        src={jumbotron}
+        alt="Banner"
+        // height={500}
+        // width={500}
+        loading="lazy"
+        fill={true}
+      />
+      <div className={styles.bannerText}>Your Banner TextI</div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import "./styles/theme.scss";
 import Header from "./components/Header";
 import styles from "./page.module.css";
+import BannerHome from "./components/BannerHome";
+import InfoCard from "./components/InfoCard";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <BannerHome />
+      <InfoCard name="Solar Panel" />
+      <h1>Energy on a Mission</h1>
+      <Footer />
     </main>
   );
 }
