8b6ba8c759e7c8d8d936b19d61cc5d79bf2fdf71
elbertwidjaja <elbertwidjaja07@gmail.com>
2024-02-07 08:36:29 +0700

    vercel error

--- app/components/Gallery/index.tsx
+++ app/components/Gallery/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import Image from "next/image";
 
 function Gallery() {
   return (
     <div className={styles.galleryContainer}>
       <h2>Galeri Inti Solar</h2>
       <p>Pemanas air real estate ,perumahan, industri dan komersial</p>
-      <div className={styles.galleryPicture}>
-        <Image />
-      </div>
+      <div className={styles.galleryPicture}></div>
     </div>
   );
 }
