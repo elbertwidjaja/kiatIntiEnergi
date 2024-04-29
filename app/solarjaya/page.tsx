6018abcd0cfc993dc2aa3e31f676d8d7a574ebcd
elbertwidjaja <elbertwidjaja07@gmail.com>
2024-04-29 23:12:55 +0700

    fix name

--- app/solarjaya/page.tsx
+++ app/solarjaya/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import MainLayout from "../components/MainLayout";
 import img from "../../public/Frame 35767.png";
-import ProductCard from "../components/ProductCard";
 import ProductHero from "./ProductHero";
 import SOLARJAYA_DATA from "./data";
 import styles from "./styles.module.scss";
+import ProductCard from "../components/ProductCard";
 
 function SolarJaya() {
   return (
