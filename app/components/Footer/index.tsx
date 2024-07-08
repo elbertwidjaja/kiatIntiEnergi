c594fea86aa050b3d330638c515f6966c0e7fd2d
elbertwidjaja <elbertwidjaja07@gmail.com>
2024-07-08 22:16:34 +0700

    Footer 2.0

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,108 +1,145 @@
-import React from "react";
-import Link from "next/link";
-import styles from "./styles.module.scss";
+"use client";
+
+import {
+  Box,
+  chakra,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
 import Image from "next/image";
-import logo from "../../../public/KiatIntiEnergi_Logo.png";
 
-const Footer = () => (
-  <footer className={styles.footerContainer}>
-    <div className={styles.flexContainer}>
-      <div className={styles.leftContainer}>
-        <section className={styles.sectionContainer}>
-          <Link href="/">
-            <span className={styles.labelText}>Home</span>
-          </Link>
-          <Link href="/profil">
-            <span className={styles.labelText}>Tentang Kami</span>
-          </Link>
-          <Link href="/#">
-            <span className={styles.labelText}>Produk</span>
-          </Link>
-          <Link href="#">
-            <span className={styles.labelText}>Layanan</span>
-          </Link>
-          <Link href="#">
-            <span className={styles.labelText}>Blog</span>
-          </Link>
-          <Link href="#">
-            <span className={styles.labelText}>Hubungi Kami</span>
-          </Link>
+const Logo = () => {
+  return (
+    <Image
+      src="https://firebasestorage.googleapis.com/v0/b/bun-homeserver.appspot.com/o/Logo%2FkiatIntiEnergi_Logo.png?alt=media&token=92f61f98-4005-4785-bff8-145a48f2ed93"
+      alt="Kiat Inti Energi Logo"
+      width={200}
+      height={200}
+    />
+  );
+};
 
-          <h2 className={`${styles.titleText} ${styles.marginTop}`}>Address</h2>
-          <div className={styles.labelText}>
-            Rukan Paris Golf Lake Residence Blok A No.27, RT.10/RW.14,
-            Cengkareng Tim., Kecamatan Cengkareng, Kota Jakarta Barat, Daerah
-            Khusus Ibukota Jakarta 11730
-          </div>
-        </section>
+const SocialButton = ({
+  children,
+  label,
+  href,
+}: {
+  children: ReactNode;
+  label: string;
+  href: string;
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
 
-        <section className={styles.sectionContainer}>
-          <h2 className={styles.titleText}>Social Media</h2>
-          <a
-            href="https://linktr.ee/kienergi_instagram"
-            className={styles.labelText}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
-          <a
-            href="https://linktr.ee/kienergi_facebook"
-            className={styles.labelText}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-          </a>
-          <a
-            href="https://linktr.ee/kienergi_tiktok"
-            className={styles.labelText}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tiktok
-          </a>
+const ListHeader = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
 
-          <h2 className={`${styles.titleText} ${styles.marginTop}`}>
-            Info Kontak
-          </h2>
-          <div className={styles.labelText}>(021) 5437-6429</div>
-          <div className={styles.labelText}>kiatintienergi@gmail.com</div>
-        </section>
-
-        <section className={styles.sectionContainer}>
-          <h2 className={styles.titleText}>Operational Hour </h2>
-          <div className={styles.labelText}>
-            Monday - Friday (08:00 - 17:00)
-          </div>
-          <div className={styles.labelText}>Saturday (08:00 - 17:00)</div>
-          <div className={styles.labelText}>Sunday (Close)</div>
-        </section>
-      </div>
-      <div className={styles.rightContainer}>
-        {/* <Image
-          src="https://firebasestorage.googleapis.com/v0/b/bun-homeserver.appspot.com/o/Logo%2Fkiatintienergi_logo.png?alt=media&token=db0737db-d932-414e-b3d1-40a1a17d8dee"
-          alt="Kiat inti energi Logo"
-          width={250}
-          height={250}
-        /> */}
-      </div>
-    </div>
-
-    <hr className={styles.line} />
-
-    <div className={styles.bottomContainer}>
-      <div className={styles.leftContainer}>
-        <p className={styles.footerText}>
-          Copyright {new Date().getFullYear()} PT. Kiat Inti Energi | All Rights
-          Reserved
-        </p>
-      </div>
-      <div className={styles.rightContainer}>
-        <p>#SOLUSIAIRBERSIHDANBERKUALITAS</p>
-      </div>
-    </div>
-  </footer>
-);
-
-export default Footer;
+export default function LargeWithNewsletter() {
+  return (
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={20}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
+          spacing={8}
+        >
+          <Stack spacing={6}>
+            <Box>
+              <Logo />
+            </Box>
+            <Text fontSize={"sm"}>
+              © 2024 PT. Kiat Inti Energi | All Rights Reserved
+            </Text>
+            <Stack direction={"row"} spacing={6}>
+              <SocialButton
+                label={"Facebook"}
+                href="https://linktr.ee/kienergi_facebook"
+              >
+                <FaFacebook />
+              </SocialButton>
+              <SocialButton
+                label={"TikTok"}
+                href="https://linktr.ee/kienergi_tiktok"
+              >
+                <FaTiktok />
+              </SocialButton>
+              <SocialButton
+                label={"Instagram"}
+                href="https://linktr.ee/kienergi_instagram"
+              >
+                <FaInstagram />
+              </SocialButton>
+            </Stack>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Company</ListHeader>
+            <Box as="a" href={"/profile"}>
+              Tentang Kami
+            </Box>
+            <Box as="a" href={"/Product"}>
+              Produk
+            </Box>
+            <Box as="a" href={"/layanan"}>
+              Layanan
+            </Box>
+            <Box as="a" href={"/blog"}>
+              Blog
+            </Box>
+            <Box as="a" href={"/hubungikami"}>
+              Hubungi Kami
+            </Box>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Info Kontak</ListHeader>
+            <Box as="text">(021) 5437-6429</Box>
+            <Box as="text">kiatintienergi@gmail.com</Box>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>Alamat</ListHeader>
+            <Box as="text">
+              Rukan Paris Golf Lake Residence Blok A No.27, RT.10/RW.14,
+              Cengkareng Tim., Kecamatan Cengkareng, Kota Jakarta Barat, Daerah
+              Khusus Ibukota Jakarta 11730
+            </Box>
+            <Stack direction={"row"}></Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
